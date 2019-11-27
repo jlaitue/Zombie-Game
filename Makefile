4893512a9ca1c27fc9d8ad668bc32196e7e13602
Julian Lechuga <julian.lechuga305@gmail.com>
2019-11-27 10:41:43 +0100

    Adding Element and Monster classes. Added Makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 CPP=g++ --std=c++11 -Wall
-all : TestBox
-TestBox : Boxint.o Test.o
-	$(CPP) -o TestBox Boxint.o Test.o
+all : Test
+Test : Test.o
+	$(CPP) -o Test Test.o
 Test.o : Test.cpp
 	$(CPP) -c Test.cpp
-Boxint.o : Boxint.cpp
-	$(CPP) -c Boxint.cpp
 clean :
-	del	*.o
+	del	*.o *.hpp.gch
---
+++ Monster.cpp
@@ -0,0 +1,22 @@
+#include "Monster.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Monster::Monster(string nameValue){
+  name = nameValue;
+  cout<<"Monster destroyed"<<endl;
+}
+
+void Monster::showMonster(){
+  std::cout<<getMonsterName()<<std::endl;
+}
+
+string Monster::getMonsterName(){
+  return name;
+}
+
+Monster::~Monster(){
+  cout<<"Monster destroyed"<<endl;
+}
