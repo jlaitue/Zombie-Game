5a1d73c14f0ecc370051c8ce04458c35f209a136
Julian Lechuga <julian.lechuga305@gmail.com>
2019-12-04 00:35:28 +0100

    Eliminated unnecessary files. Cleaned code Test.cpp by eliminating unnecessary functions. Added function for random creation of board. Created folder for created boards

--- Test.cpp
+++ Test.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <array>
 #include <fstream>
 #include <ctime>
 #include "Element.hpp"
@@ -9,41 +7,17 @@
 using namespace std;
 
 const size_t n = 20;
-const size_t m = 41;
+const size_t m = 40;
 
-void getGridSize(int dimensions[2]){
-  // array  dimensions;
-  string line;
-  int rows=0,columns=0;
-
-  fstream inFile;
-  inFile.open("tableau.txt");
-
-  if (!inFile) {
-    cerr << "Unable to open file datafile.txt";
-    exit(1);   // call system to stop
-  }
-
-  while (getline(inFile, line)) {
-    rows++;
-    }
-    columns = line.size();
-    inFile.close();
-    dimensions[0] = rows;
-    dimensions[1] = columns;
-
-    // return dimensions;
-}
-
-void printMatrix(string matrix[][m], size_t n, size_t m){
-  for (size_t i = 0; i < n; i++) {
-    for (size_t j = 0; j < m; j++) {
-      cout<<matrix[i][j];
+void displayBoard(string matrix[][m], size_t n, size_t m){
+    for (size_t i = 0; i < n; i++) {
+      for (size_t j = 0; j < m; j++) {
+        cout<<matrix[i][j];
+      }
+      cout<<"\n";
     }
     cout<<"\n";
   }
-  cout<<"\n";
-}
 
 void movement(string matrix[][m], size_t n, size_t m){
   string move = "";
@@ -53,96 +27,96 @@ void movement(string matrix[][m], size_t n, size_t m){
   matrix[rowPosition][colPosition] = "O";
   cout<<"Intial position (x,y) =  "<<rowPosition<<", "<<colPosition<<endl;
   cout<<"Initialized matrix: \n\n";
-  printMatrix(matrix,n,m);
+  displayBoard(matrix,n,m);
   while (move != "STOP") {
     cout<<"\nEnter move: ";
     cin>>move;
 
-    if (move == "w"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "w" or move == "W"){
+      matrix[rowPosition][colPosition] = ".";
       rowPosition --;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving UP"<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "s"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "s" or move == "S"){
+      matrix[rowPosition][colPosition] = ".";
       rowPosition ++;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving DOWN"<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "d"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "d" or move == "D"){
+      matrix[rowPosition][colPosition] = ".";
       colPosition++;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving RIGHT"<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "a"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "a" or move == "A"){
+      matrix[rowPosition][colPosition] = ".";
       colPosition--;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving LEFT"<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "e"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "e" or move == "E"){
+      matrix[rowPosition][colPosition] = ".";
       colPosition++;
       rowPosition--;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving "<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "q"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "q" or move == "Q"){
+      matrix[rowPosition][colPosition] = ".";
       colPosition--;
       rowPosition--;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving "<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "z"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "z" or move == "Z"){
+      matrix[rowPosition][colPosition] = ".";
       colPosition--;
       rowPosition++;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving "<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
 
-    if (move == "c"){
-      matrix[rowPosition][colPosition] = "X";
+    if (move == "c" or move == "C"){
+      matrix[rowPosition][colPosition] = ".";
       colPosition++;
       rowPosition++;
       matrix[rowPosition][colPosition] = "O";
       cout<<"Moving "<<endl;
       cout<<"(x,y) = ("<<rowPosition<<", "<<colPosition<<")"<<endl;
-      printMatrix(matrix,n,m);
+      displayBoard(matrix,n,m);
     }
-    if (move == "x"){
+    if (move == "x" or move == "X"){
       move = "STOP";
     }
   }
 }
 
-void fileReading(string filename, string matrix[][m]){
+void boardReading(string filename, string matrix[][m]){
   string line;
   int rows=0,columns=0;
-  int xcount = 0, powcount=0, diamcount=0, monscount=0, whitecount=0;
+  int xcount = 0, powcount=0, diamcount=0, monscount=0, whitecount=0, doorscount = 0;
 
   fstream inFile;
   inFile.open(filename);
@@ -151,7 +125,7 @@ void fileReading(string filename, string matrix[][m]){
     cerr << "Unable to open file datafile.txt";
     exit(1);   // call system to stop
   }
-
+  cout << "Reading board file: "<<filename<<"\n\n";
   while (getline(inFile, line)) {
         cout<<"Analyzing row: "<<rows<<endl;
         // using printf() in all tests for consistency
@@ -180,13 +154,13 @@ void fileReading(string filename, string matrix[][m]){
 
           }
           else if (line[i] == 'M'){
-            cout<<"FOUND A MONSTER";
+            cout<<"FOUND A MONSTER ";
             cout<<"Coordinates: "<<rows<<", "<<i<<"\n";
             matrix[rows][i] = "M";
             monscount++;
           }
           else if (line[i] == ' '){
-            cout<<"FOUND EMPTY SPACE";
+            cout<<"FOUND EMPTY SPACE ";
             cout<<"Coordinates: "<<rows<<", "<<i<<"\n";
             matrix[rows][i] = " ";
             whitecount++;
@@ -196,123 +170,176 @@ void fileReading(string filename, string matrix[][m]){
             cout<<"Coordinates: "<<rows<<", "<<i<<"\n";
             matrix[rows][i] = "J";
           }
+          else if (line[i] == '/'){
+            cout<<"FOUND DOOR ";
+            cout<<"Coordinates: "<<rows<<", "<<i<<"\n";
+            matrix[rows][i] = "/";
+          }
         }
-        printf("\n");
+        cout<<"\n";
         rows ++;
     }
     cout<<"Amount X "<<xcount<<endl;
     cout<<"Amount * "<<powcount<<endl;
     cout<<"Amount $ "<<diamcount<<endl;
     cout<<"Amount M "<<monscount<<endl;
+    cout<<"Amount / "<<doorscount<<endl;
     cout<<"Amount ' ' "<<whitecount<<endl;
 
-    columns = line.size();
+    /* This value of columns is zero because when the .txt file is saved, a new
+    line is added at the end. The files have 21 lines, being the last one empty
+    which is why the value size of that last line is 0
+    */
+    // columns = line.size();
+    columns = m;
     cout<<"Total Rows: "<<rows<<endl;
     cout<<"Total Columns: "<<columns<<endl;
     cout<<"\n\n\n";
 }
 
-void createMatrix(){
-  ofstream myfile ("EX1.txt");
+void positionObjects(int nDiam, int nMons, int nDoors, int nStars, string matrix[][m]){
+  srand (time(NULL));
+
+  while(nDiam != 0){
+    int row = rand() %19 + 1;
+    int column = rand() %39 + 1;
+    if(matrix[row][column] == " "){
+      matrix[row][column] = "$";
+      nDiam --;
+    }
+  }
+
+  while(nMons != 0){
+    int row = rand() %19 + 1;
+    int column = rand() %39 + 1;
+    if(matrix[row][column] == " "){
+      matrix[row][column] = "M";
+      nMons --;
+    }
+  }
+
+  while(nStars != 0){
+    int row = rand() %19 + 1;
+    int column = rand() %39 + 1;
+    if(matrix[row][column] == " "){
+      matrix[row][column] = "*";
+      nStars --;
+    }
+  }
+
+  while(nDoors != 0){
+    int doorCoordCase = rand() %4 + 1;
+    switch (doorCoordCase) {
+      case 1:{
+        int row = rand() %18 + 1;
+        int column = 0;
+        matrix[row][column] = "/";
+        break;
+      }
+      case 2:{
+        int row = 0;
+        int column = rand() %38 + 1;
+        matrix[row][column] = "/";
+        break;
+      }
+      case 3:{
+        int row = rand() %18 + 1;
+        int column = 39;
+        matrix[row][column] = "/";
+        break;
+      }
+      case 4:{
+        int row = 19;
+        int column = rand() %38 + 1;
+        matrix[row][column] = "/";
+        break;
+      }
+    }
+    nDoors--;
+  }
+}
+
+void createTheMatrix(int level, string filename){
+  string matrix[n][m];
+  ofstream myfile ("boards/"+ filename);
+
+// Creation of string matrix with elements of board
+  for (size_t i = 0; i < n; i++) {
+      for (size_t j = 0; j < m; j++) {
+        if(i==0 or i == n-1){
+          matrix[i][j] = "X";
+        }
+        else if(i > 0 and (j == 0 or j == m-1)){
+          matrix[i][j] = "X";
+        }
+        else if(i > 0 and (j > 0 and j < m-1)){
+          matrix[i][j] = " ";
+        }
+      }
+    }
+
+/*
+Positioning of different amount of elements depending on difficulty level
+chosen
+*/
+switch (level) {
+  case 1:
+    {
+      cout<<"Creation of board of level 1\n\n";
+      int nDiam = 5, nMons = 1, nStars = 0, nDoors = 5;
+      positionObjects(nDiam, nMons, nDoors, nStars, matrix);
+      break;
+    }
+  case 2:
+    {
+      cout<<"Creation of board of level 2\n\n";
+      int nDiam = 5, nMons = 1, nStars = 1, nDoors = 4;
+      positionObjects(nDiam, nMons, nDoors, nStars, matrix);
+      break;
+    }
+  case 3:
+    {
+      cout<<"Creation of board of level 3\n\n";
+      int nDiam = 4, nMons = 3, nStars = 2, nDoors = 3;
+      positionObjects(nDiam, nMons, nDoors, nStars, matrix);
+      break;
+    }
+  case 4:
+    {
+      cout<<"Creation of board of level 4\n\n";
+      int nDiam = 4, nMons = 4, nStars = 3, nDoors = 2;
+      positionObjects(nDiam, nMons, nDoors, nStars, matrix);
+      break;
+    }
+  case 5:
+    {
+      cout<<"Creation of board of level 5\n\n";
+      int nDiam = 8, nMons = 5, nStars = 4, nDoors = 1;
+      positionObjects(nDiam, nMons, nDoors, nStars, matrix);
+      break;
+    }
+}
+
+  displayBoard(matrix, n, m);
+
+// Writting of matrix previously created to .txt file
   if (myfile.is_open())
   {
-    for (size_t i = 0; i < 20; i++) {
-      myfile << "XXXXXXXXXXXXXXXXXXXX\n";
+    cout<<"Writting to board file: boards/"<< filename<<" ..."<<endl;
+    for (size_t i = 0; i < n; i++) {
+      for (size_t j = 0; j < m; j++) {
+        myfile<<matrix[i][j];
+      }
+      myfile<<"\n";
     }
     myfile.close();
+    cout<<"Done writting."<<endl;
   }
   else cout << "Unable to open file";
 }
 
-void createBoard(int nDiam){
-  int n = 20, m = 40;
-  string line = "", line2 = "X";
-
-  srand (time(NULL));
-  // cout<<row<<endl;
-  // cout<<column<<endl;
-
-  ofstream outfile ("board.txt");
-  int row = rand() %18 + 1;
-  int column = rand() %38 + 1;
-
-  cout<<row<<endl;
-  cout<<column<<endl;
-
-  for (int i = 0; i < m ; i++) {
-    line = line + "X";
-    if(i>1){
-      line2 = line2 + " ";
-    }
-  }
-
-  line = line + "X\n";
-  line2 = line2 + " X\n";
-  cout<< line<<endl;
-  cout<< line2<<endl;
-
-  if (outfile.is_open())
-  {
-    cout << "Writting to file"<<endl;
-
-    for (int i = 0; i < n; i++) {
-      if(i == 0 or i == n-1){
-        outfile << line;
-      }
-      else if (i == row and nDiam != 0){
-        string elementos = "X";
-
-        for (int k = 0; k < m ; k++) {
-          if(k!=column){
-            elementos = elementos + " ";
-          }
-          else {
-            elementos = elementos + "$";
-            nDiam --;
-          }
-        }
-        elementos = elementos + "X\n";
-        outfile<<elementos;
-      }
-      else {
-        outfile<<line2;
-      }
-    }
-
-    // while (nDiam != 0) {
-    //   for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < m; j++) {
-    //       if (i == row) {
-    //
-    //         outfile << "$";
-    //         nDiam --;
-    //       }
-    //     }
-    //   }
-    // }
-
-    outfile.close();
-    cout << "Done writting."<<endl;
-  }
-  else cout << "Unable to open file";
-}
-
-void randomValues(){
-  srand (time(NULL));
-  int row = rand() %19 + 1;
-  int column = rand() %39 + 1;
-  cout<<row<<endl;
-  cout<<column<<endl;
-}
 int main() {
   string matrix[n][m];
-
-  // for (size_t i = 0; i < n; i++) {
-  //   for (size_t j = 0; j < m; j++) {
-  //     matrix[i][j] = "X";
-  //   }
-  // }
-
   // Element element1("static", 0, 1, "$");
   // Element element2("static", 1, 1, "X");
   //
@@ -325,15 +352,15 @@ int main() {
   // monster1.showMonster();
   // monster2.showMonster();
 
-  // fileReading("board.txt", matrix);
-  // printMatrix(matrix,n,m);
-  // movement(matrix,n,m);
 
-  string cul = "HOLA";
-  cout<<cul[1]<<endl;
-  cul.replace(1,1,"X");
-  cout<<cul<<endl;¡
-  createBoard(3);
-  // randomValues();¡
+  // createTheMatrix(1, "prueba1.txt");
+  // createTheMatrix(2, "prueba2.txt");
+  // createTheMatrix(3, "prueba3.txt");
+  // createTheMatrix(4, "prueba4.txt");
+  // createTheMatrix(5, "prueba5.txt");
+  createTheMatrix(3, "prueba6.txt");
+  // boardReading("boards/prueba1.txt", matrix);
+  // displayBoard(matrix,n,m);
+  // movement(matrix,n,m);
   return 0;
 }
